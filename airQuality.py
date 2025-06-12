df1b4f6a314f905f8ecd2ce55974158506aa43ad
brjatoba92 <brjatoba92@gmail.com>
2025-06-12 11:35:16 -0300

    Criação do gitignore para não enviar o .venv para o github, instalação e importacao das bibliotecas

---
+++ airQuality.py
@@ -0,0 +1,11 @@
+# Importação das dependencias
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from datetime import datetime, timedelta
+import warnings
+warnings.filterwarnings('ignore')
